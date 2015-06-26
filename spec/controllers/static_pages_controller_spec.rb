35f686ec5e546cf32997f94cacc42ee604be99f4
Ryan MacGillivray <rmacg89@gmail.com>
2015-06-26 19:19:17 +0100

    initial tests for static pages

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+
+  describe "GET #home" do
+    it "returns http success" do
+      get :home
+      expect(response).to have_http_status(:success)
+      expect(assigns(:page_title)).to eq('Home')
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
