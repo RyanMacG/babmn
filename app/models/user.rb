55cf255ef362cf398715f61db035618fa101f5c2
Ryan MacGillivray <rmacg89@gmail.com>
2015-06-27 11:57:00 +0100

    updated validations to check format of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  validates_presence_of :name, :email
+  validates_presence_of :name
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email, with: VALID_EMAIL_REGEX, message: 'Must be a valid email address'
 end
