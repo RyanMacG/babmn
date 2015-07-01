d12fadb64d0fd86318235e3bbf3a47dd8ef3b182
Paul Beattie <paul@paul-beattie.com>
2015-07-01 23:10:06 +0100

    Updated user factory to handle password creation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
     name 'Foo Bar'
+    sequence(:password) { |n| "passwordNumber#{n}" }
     sequence(:email) { |n| "foo_bar_#{n}@example.com" }
   end
 end
